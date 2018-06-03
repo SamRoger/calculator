a734d11f6751c98b95daeabf8ed692ddb8b4ddca
Samuel Almeida <samuelalmeida@Samuels-MacBook-Pro.local>
2018-06-03 09:44:02 -0400

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,94 @@
+$("document").ready(function(){
+
+
+// function calculate(){
+// var amount = document.getElementsByClassName("amount")[0];
+// 	$("button").click(function(){
+// 		if(amount.value() === "integer"){
+// 			alert("yes!")
+// 		} else {
+// 			alert("no!")
+// 		}
+// 	})
+// }
+// calculate();
+
+
+function calculate(){
+$(".button").click(function(){
+	var amount = document.getElementsByClassName("amount")[0];
+	var payout = document.getElementsByClassName("payout")[0];
+	var result = document.getElementsByClassName("result")[0];
+	var hourlyRate = document.getElementsByClassName("hourlyRate")[0];
+//----------------------------------------------------------//
+	var hourlyRate = hourlyRate.value
+	var payout = payout.value * hourlyRate
+	var hourlyRate = hourlyRate.value
+	var weekAmount = amount.value
+	var income = parseInt(weekAmount) * .11
+	var payAfterHelp = parseInt(income) - parseInt(payout)
+	var deductTax = payAfterHelp * .27
+	var incomeAfterTaxes = parseInt(payAfterHelp) - parseInt(deductTax)
+	var dailyAmount = parseInt(weekAmount) / 5
+//----------------------------------------------------------//
+	$(".result").html("$" + incomeAfterTaxes)
+	$(".totalEarn").html(" $" + income)
+	$(".dailyAmount").html(" $" + dailyAmount)
+	$(".weekPay").html(" $" + payout)
+	$(".tax").html(" $" + deductTax)
+//----------------------------------------------------------//
+
+//----------------------------------------------------------//
+	console.log("gross income", income)
+	console.log("payout", payout)
+	console.log("incomeAfterTaxes", incomeAfterTaxes)
+	console.log("dailyAmount", dailyAmount)
+	})
+}
+calculate();
+//==================================================================//
+
+$(".button").on("click", function(){
+	$(".hideForm").fadeOut(500)
+	$(".display").fadeIn(500)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
